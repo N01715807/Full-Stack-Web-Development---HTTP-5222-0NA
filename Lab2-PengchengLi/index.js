99d9fc1f66048cba4f8be951cbd23610a519cef6
pengchengli <n01715807@humber.ca>
2025-09-13 21:17:04 -0400

    Full-Stack-Web-Development---HTTP-5222-0NA

--- Lab2-PengchengLi/index.js
+++ Lab2-PengchengLi/index.js
@@ -1,3 +1,22 @@
 import express from "express";
 import path from "path";
+import { title } from "process";
 
+const app = express();
+const __dirname = import.meta.dirname
+const port = process.env.PORT || 8888;
+
+app.set("views", path.join(__dirname,"templates"));
+app.set("view engine","pug")
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>{
+    res.status(200).render("index",{title:"Home|E-commerce Store"})
+});
+app.get("/about",(req,res)=>{
+    res.status(200).render("about",{title:"AboutUS|E-commerce Store"})
+});
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
